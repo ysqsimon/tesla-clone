9701e6ac406de310f13758b6808151f9cbee4d54
ysqsimon <ysqsimon@hotmail.com>
2023-03-21 21:45:40 +0800

    updated github pages base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tesla-clone/",
   plugins: [vue(), vuetify()],
   resolve: {
     alias: {
