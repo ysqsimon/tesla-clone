ca704cd32179d53ef2d06c5ec78c4df125e2e666
ysqsimon <ysqsimon@hotmail.com>
2023-03-21 22:38:15 +0800

    no message

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/home.vue";
 
 export default createRouter({
+  base: "/tesla-clone/",
   history: createWebHistory(),
   routes: [
     {
